81ec4ec983aad874afc19a98387033b336a56dba
majouve <98130904+majouve@users.noreply.github.com>
2022-01-20 18:21:53 -0500

    Add files via upload

---
+++ TestScript.py
@@ -0,0 +1,12 @@
+
+# imports
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Load in coin group and coin find files
+CoinFinds = pd.read_csv("/Users/maryalicejouve/Documents/Work/FLAME/CoinData/CoinFinds.csv")
+CoinGroups = pd.read_csv("/Users/maryalicejouve/Documents/Work/FLAME/CoinData/CoinGroups.csv")
+
+print("Hello")
